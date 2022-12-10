f359b67f0aed755c93bb278c66271b9f1804b747
William Shivers <will@Williams-MBP.attlocal.net>
2022-12-09 22:59:37 -0600

    Add function to read single line

--- src/util/fileHandler.ts
+++ src/util/fileHandler.ts
@@ -1,5 +1,11 @@
 import {readFileSync} from 'fs';
 
+export function readInput(filepath: string): string {
+  const data = readFileSync(filepath, 'utf-8');
+
+  return data;
+}
+
 export function getInputAsArray(filepath: string): string[] {
   const data = readFileSync(filepath, 'utf-8');
   const dataArr = data.split(/\n/g);
